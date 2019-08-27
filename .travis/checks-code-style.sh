ddcfe70dcdae58de4de401113ab722e27cdcab0a
Dominik Salvet <dominik.salvet@gmail.com>
2019-08-27 21:56:10 +0200

    Improve code style of checks-code-style.sh

--- .travis/checks-code-style.sh
+++ .travis/checks-code-style.sh
@@ -6,14 +6,31 @@
 # https://github.com/dominiksalvet/gitpack
 #-------------------------------------------------------------------------------
 
-# check whether all lines have at most 80 characters
+#-------------------------------------------------------------------------------
+# CONFIGURATION
+#-------------------------------------------------------------------------------
+
+MAX_LINE_LENGTH=80 # maximum number of characters per each line
+MAX_FUNCTION_LENGTH=20 # maximum number of lines per each function
+IGNORED_FUNCTIONS='init_constants init_strings' # function length exceptions
+
+#-------------------------------------------------------------------------------
+# LINE LENGTH
+#-------------------------------------------------------------------------------
+
 # shellcheck disable=SC2016
 while read -r line; do
-    test "${#line}" -le 80 || return
+    if [ "${#line}" -gt "$MAX_LINE_LENGTH" ]; then # check line length
+        echo "src/gitpack must have lines with $MAX_LINE_LENGTH characters at most" >&2
+        return 1
+    fi
 done < src/gitpack &&
 
-# check whether all functions have at most 20 lines
-IGNORED_FUNCTIONS='init_constants init_strings' &&
+#-------------------------------------------------------------------------------
+# FUNCTION LENGTH
+#-------------------------------------------------------------------------------
+
+# transforms a given shell script into '<function> <lines>' per line output
 AWK_PROGRAM='{ if (/\(\)/) {
     printf $1" "
     lines=1
@@ -23,15 +40,21 @@ AWK_PROGRAM='{ if (/\(\)/) {
     lines++
 } }' &&
 
+# use '(' as a field separator to make creating the final output easier
 awk_out="$(awk -F '(' "$AWK_PROGRAM" src/gitpack)" &&
 echo "$awk_out" |
 while read -r line; do
-    function="${line%% *}" &&
-    lines="${line##* }" &&
-    for ignored in $IGNORED_FUNCTIONS; do
-        if [ "$function" = "$ignored" ]; then
-            lines=20
+    function="${line%% *}" # extract function name
+    lines="${line##* }" # extract function length in lines
+
+    for ignored_function in $IGNORED_FUNCTIONS; do # exclude ignored functions
+        if [ "$function" = "$ignored_function" ]; then
+            lines="$MAX_FUNCTION_LENGTH"
         fi
-    done &&
-    test "$lines" -le 20 || return
+    done
+
+    if [ "$lines" -gt "$MAX_FUNCTION_LENGTH" ]; then # check function length
+        echo "src/gitpack must have functions with $MAX_FUNCTION_LENGTH lines at most" >&2
+        return 1
+    fi
 done
