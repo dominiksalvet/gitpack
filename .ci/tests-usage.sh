7521297efee33dc39c71f23074bd48850c54766b
Dominik Salvet <dominik.salvet@gmail.com>
2019-09-10 00:03:36 +0200

    Improve status file removal test

--- .ci/tests-usage.sh
+++ .ci/tests-usage.sh
@@ -19,45 +19,49 @@ echo test4 >&2 && gitpack_out="$(gitpack status github.com/dominiksalvet/vhdldep
 echo "$gitpack_out" | grep '^\[nothing\]' &&
 echo test5 >&2 && gitpack_out="$(gitpack install github.com/dominiksalvet/vhdldep=2.1.0)" &&
 echo "$gitpack_out" | grep '^\[install\]' &&
-echo test6 >&2 && rm "$HOME"/.local/share/gitpack/status && # should be recovered from a backup
-echo test7 >&2 && gitpack_out="$(gitpack install github.com/dominiksalvet/vhdldep=2.1.0)" &&
+echo test6 >&2 && gitpack_out="$(gitpack install github.com/dominiksalvet/vhdldep=2.1.0)" &&
 echo "$gitpack_out" | grep '^\[installed\]' &&
-echo test8 >&2 && gitpack_out="$(gitpack status github.com/dominiksalvet/vhdldep=2.1.0)" &&
+echo test7 >&2 && gitpack_out="$(gitpack status github.com/dominiksalvet/vhdldep=2.1.0)" &&
 echo "$gitpack_out" | grep '^\[ok\]' &&
-echo test9 >&2 && gitpack_out="$(gitpack list)" &&
+echo test8 >&2 && gitpack_out="$(gitpack list)" &&
 echo "$gitpack_out" | grep -F 'github.com/dominiksalvet/vhdldep' &&
 
 # update to the latest version
-echo test10 >&2 && gitpack_out="$(gitpack status github.com/dominiksalvet/vhdldep)" &&
+echo test9 >&2 && gitpack_out="$(gitpack status github.com/dominiksalvet/vhdldep)" &&
 echo "$gitpack_out" | grep '^\[older\]' &&
+echo test10 >&2 && rm "$HOME"/.local/share/gitpack/status && # should be recovered from a backup
 echo test11 >&2 && gitpack_out="$(gitpack install github.com/dominiksalvet/vhdldep)" &&
 echo "$gitpack_out" | grep '^\[update\]' &&
-echo test12 >&2 && gitpack_out="$(gitpack install github.com/dominiksalvet/vhdldep)" &&
+echo test12 >&2 && rm "$HOME"/.local/share/gitpack/status &&
+echo test13 >&2 && gitpack_out="$(gitpack install github.com/dominiksalvet/vhdldep)" &&
 echo "$gitpack_out" | grep '^\[installed\]' &&
-echo test13 >&2 && gitpack_out="$(gitpack status github.com/dominiksalvet/vhdldep)" &&
+echo test14 >&2 && rm "$HOME"/.local/share/gitpack/status &&
+echo test15 >&2 && gitpack_out="$(gitpack status github.com/dominiksalvet/vhdldep)" &&
 echo "$gitpack_out" | grep '^\[ok\]' &&
-echo test14 >&2 && gitpack_out="$(gitpack list)" &&
+echo test16 >&2 && rm "$HOME"/.local/share/gitpack/status &&
+echo test17 >&2 && gitpack_out="$(gitpack list)" &&
 echo "$gitpack_out" | grep -F 'github.com/dominiksalvet/vhdldep' &&
+echo test18 >&2 && rm "$HOME"/.local/share/gitpack/status &&
 
 # downgrade back to 2.1.0
-echo test15 >&2 && gitpack_out="$(gitpack status github.com/dominiksalvet/vhdldep=2.1.0)" &&
+echo test19 >&2 && gitpack_out="$(gitpack status github.com/dominiksalvet/vhdldep=2.1.0)" &&
 echo "$gitpack_out" | grep '^\[newer\]' &&
-echo test16 >&2 && gitpack_out="$(gitpack install github.com/dominiksalvet/vhdldep=2.1.0)" &&
+echo test20 >&2 && gitpack_out="$(gitpack install github.com/dominiksalvet/vhdldep=2.1.0)" &&
 echo "$gitpack_out" | grep '^\[downgrade\]' &&
-echo test17 >&2 && gitpack_out="$(gitpack install github.com/dominiksalvet/vhdldep=2.1.0)" &&
+echo test21 >&2 && gitpack_out="$(gitpack install github.com/dominiksalvet/vhdldep=2.1.0)" &&
 echo "$gitpack_out" | grep '^\[installed\]' &&
-echo test18 >&2 && gitpack_out="$(gitpack status github.com/dominiksalvet/vhdldep=2.1.0)" &&
+echo test22 >&2 && gitpack_out="$(gitpack status github.com/dominiksalvet/vhdldep=2.1.0)" &&
 echo "$gitpack_out" | grep '^\[ok\]' &&
-echo test19 >&2 && gitpack_out="$(gitpack list)" &&
+echo test23 >&2 && gitpack_out="$(gitpack list)" &&
 echo "$gitpack_out" | grep -F 'github.com/dominiksalvet/vhdldep' &&
 
 # uninstall vhdldep
-echo test20 >&2 && gitpack_out="$(gitpack uninstall github.com/dominiksalvet/vhdldep)" &&
+echo test24 >&2 && gitpack_out="$(gitpack uninstall github.com/dominiksalvet/vhdldep)" &&
 echo "$gitpack_out" | grep '^\[uninstall\]' &&
-echo test21 >&2 && gitpack_out="$(gitpack uninstall github.com/dominiksalvet/vhdldep)" &&
+echo test25 >&2 && gitpack_out="$(gitpack uninstall github.com/dominiksalvet/vhdldep)" &&
 echo "$gitpack_out" | grep '^\[uninstalled\]' &&
-echo test22 >&2 && gitpack_out="$(gitpack status github.com/dominiksalvet/vhdldep)" &&
+echo test26 >&2 && gitpack_out="$(gitpack status github.com/dominiksalvet/vhdldep)" &&
 echo "$gitpack_out" | grep '^\[nothing\]' &&
 
 # clean cache again
-echo test23 >&2 && gitpack clean
+echo test27 >&2 && gitpack clean
