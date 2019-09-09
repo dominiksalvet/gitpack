e3bc1a735bc2b913380ea5c6704037b0477e944c
Dominik Salvet <dominik.salvet@gmail.com>
2019-09-09 23:50:50 +0200

    Remove status file in a test, should be recovered

--- .ci/tests-usage.sh
+++ .ci/tests-usage.sh
@@ -19,44 +19,45 @@ echo test4 >&2 && gitpack_out="$(gitpack status github.com/dominiksalvet/vhdldep
 echo "$gitpack_out" | grep '^\[nothing\]' &&
 echo test5 >&2 && gitpack_out="$(gitpack install github.com/dominiksalvet/vhdldep=2.1.0)" &&
 echo "$gitpack_out" | grep '^\[install\]' &&
-echo test6 >&2 && gitpack_out="$(gitpack install github.com/dominiksalvet/vhdldep=2.1.0)" &&
+echo test6 >&2 && rm "$HOME"/.local/share/gitpack/status && # should be recovered from a backup
+echo test7 >&2 && gitpack_out="$(gitpack install github.com/dominiksalvet/vhdldep=2.1.0)" &&
 echo "$gitpack_out" | grep '^\[installed\]' &&
-echo test7 >&2 && gitpack_out="$(gitpack status github.com/dominiksalvet/vhdldep=2.1.0)" &&
+echo test8 >&2 && gitpack_out="$(gitpack status github.com/dominiksalvet/vhdldep=2.1.0)" &&
 echo "$gitpack_out" | grep '^\[ok\]' &&
-echo test8 >&2 && gitpack_out="$(gitpack list)" &&
+echo test9 >&2 && gitpack_out="$(gitpack list)" &&
 echo "$gitpack_out" | grep -F 'github.com/dominiksalvet/vhdldep' &&
 
 # update to the latest version
-echo test9 >&2 && gitpack_out="$(gitpack status github.com/dominiksalvet/vhdldep)" &&
+echo test10 >&2 && gitpack_out="$(gitpack status github.com/dominiksalvet/vhdldep)" &&
 echo "$gitpack_out" | grep '^\[older\]' &&
-echo test10 >&2 && gitpack_out="$(gitpack install github.com/dominiksalvet/vhdldep)" &&
-echo "$gitpack_out" | grep '^\[update\]' &&
 echo test11 >&2 && gitpack_out="$(gitpack install github.com/dominiksalvet/vhdldep)" &&
+echo "$gitpack_out" | grep '^\[update\]' &&
+echo test12 >&2 && gitpack_out="$(gitpack install github.com/dominiksalvet/vhdldep)" &&
 echo "$gitpack_out" | grep '^\[installed\]' &&
-echo test12 >&2 && gitpack_out="$(gitpack status github.com/dominiksalvet/vhdldep)" &&
+echo test13 >&2 && gitpack_out="$(gitpack status github.com/dominiksalvet/vhdldep)" &&
 echo "$gitpack_out" | grep '^\[ok\]' &&
-echo test13 >&2 && gitpack_out="$(gitpack list)" &&
+echo test14 >&2 && gitpack_out="$(gitpack list)" &&
 echo "$gitpack_out" | grep -F 'github.com/dominiksalvet/vhdldep' &&
 
 # downgrade back to 2.1.0
-echo test14 >&2 && gitpack_out="$(gitpack status github.com/dominiksalvet/vhdldep=2.1.0)" &&
+echo test15 >&2 && gitpack_out="$(gitpack status github.com/dominiksalvet/vhdldep=2.1.0)" &&
 echo "$gitpack_out" | grep '^\[newer\]' &&
-echo test15 >&2 && gitpack_out="$(gitpack install github.com/dominiksalvet/vhdldep=2.1.0)" &&
-echo "$gitpack_out" | grep '^\[downgrade\]' &&
 echo test16 >&2 && gitpack_out="$(gitpack install github.com/dominiksalvet/vhdldep=2.1.0)" &&
+echo "$gitpack_out" | grep '^\[downgrade\]' &&
+echo test17 >&2 && gitpack_out="$(gitpack install github.com/dominiksalvet/vhdldep=2.1.0)" &&
 echo "$gitpack_out" | grep '^\[installed\]' &&
-echo test17 >&2 && gitpack_out="$(gitpack status github.com/dominiksalvet/vhdldep=2.1.0)" &&
+echo test18 >&2 && gitpack_out="$(gitpack status github.com/dominiksalvet/vhdldep=2.1.0)" &&
 echo "$gitpack_out" | grep '^\[ok\]' &&
-echo test18 >&2 && gitpack_out="$(gitpack list)" &&
+echo test19 >&2 && gitpack_out="$(gitpack list)" &&
 echo "$gitpack_out" | grep -F 'github.com/dominiksalvet/vhdldep' &&
 
 # uninstall vhdldep
-echo test19 >&2 && gitpack_out="$(gitpack uninstall github.com/dominiksalvet/vhdldep)" &&
-echo "$gitpack_out" | grep '^\[uninstall\]' &&
 echo test20 >&2 && gitpack_out="$(gitpack uninstall github.com/dominiksalvet/vhdldep)" &&
+echo "$gitpack_out" | grep '^\[uninstall\]' &&
+echo test21 >&2 && gitpack_out="$(gitpack uninstall github.com/dominiksalvet/vhdldep)" &&
 echo "$gitpack_out" | grep '^\[uninstalled\]' &&
-echo test21 >&2 && gitpack_out="$(gitpack status github.com/dominiksalvet/vhdldep)" &&
+echo test22 >&2 && gitpack_out="$(gitpack status github.com/dominiksalvet/vhdldep)" &&
 echo "$gitpack_out" | grep '^\[nothing\]' &&
 
 # clean cache again
-echo test22 >&2 && gitpack clean
+echo test23 >&2 && gitpack clean
