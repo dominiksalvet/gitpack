6aff13c7d0a6a45f8748173179062773226aecd8
Dominik Salvet <dominik.salvet@gmail.com>
2019-09-10 15:27:46 +0200

    Add files tests and improve the whole test suite

---
+++ .ci/tests-files.sh
@@ -0,0 +1,127 @@
+#!/bin/sh
+
+#-------------------------------------------------------------------------------
+# Copyright 2019 Dominik Salvet
+# SPDX-License-Identifier: MIT
+# https://github.com/dominiksalvet/gitpack
+#-------------------------------------------------------------------------------
+# DESCRIPTION:
+#   This test checks the correct existence of GitPack files.
+#-------------------------------------------------------------------------------
+
+# clean cache
+echo test1 >&2 && gitpack clean &&
+# check files
+echo test3 >&2 && test -d "$HOME"/.local/share/gitpack/ &&
+echo test4 >&2 && test -r "$HOME"/.local/share/gitpack/gitpack.log &&
+echo test5 >&2 && test -w "$HOME"/.local/share/gitpack/gitpack.log &&
+echo test6 >&2 && test -r "$HOME"/.local/share/gitpack/status &&
+echo test7 >&2 && test -w "$HOME"/.local/share/gitpack/status &&
+echo test8 >&2 && test -r "$HOME"/.local/share/gitpack/status.bak &&
+echo test9 >&2 && test -w "$HOME"/.local/share/gitpack/status.bak &&
+echo test10 >&2 && test -d "$HOME"/.cache/gitpack/ &&
+echo test11 >&2 && test ! -d "$HOME"/.cache/gitpack/repo/ &&
+
+# install an example project, vhdldep, in version 2.1.0 (not the latest release)
+echo test12 >&2 && gitpack status github.com/dominiksalvet/vhdldep=2.1.0 &&
+gitpack install github.com/dominiksalvet/vhdldep=2.1.0 &&
+gitpack install github.com/dominiksalvet/vhdldep=2.1.0 &&
+gitpack status github.com/dominiksalvet/vhdldep=2.1.0 &&
+gitpack list &&
+# check files
+echo test13 >&2 && test -d "$HOME"/.local/share/gitpack/ &&
+echo test14 >&2 && test -r "$HOME"/.local/share/gitpack/gitpack.log &&
+echo test15 >&2 && test -w "$HOME"/.local/share/gitpack/gitpack.log &&
+echo test16 >&2 && test -r "$HOME"/.local/share/gitpack/status &&
+echo test17 >&2 && test -w "$HOME"/.local/share/gitpack/status &&
+echo test18 >&2 && test -r "$HOME"/.local/share/gitpack/status.bak &&
+echo test19 >&2 && test -w "$HOME"/.local/share/gitpack/status.bak &&
+echo test20 >&2 && test -d "$HOME"/.cache/gitpack/ &&
+echo test21 >&2 && test -d "$HOME"/.cache/gitpack/repo/ &&
+echo test22 >&2 && test -d "$HOME"/.cache/gitpack/repo/github.com/dominiksalvet/ &&
+echo test23 >&2 && test -d "$HOME"/.cache/gitpack/repo/github.com/dominiksalvet/vhdldep/ &&
+
+# update to the latest version
+echo test24 >&2 && gitpack status github.com/dominiksalvet/vhdldep &&
+gitpack install github.com/dominiksalvet/vhdldep &&
+gitpack install github.com/dominiksalvet/vhdldep &&
+gitpack status github.com/dominiksalvet/vhdldep &&
+gitpack list &&
+# check files
+echo test25 >&2 && test -d "$HOME"/.local/share/gitpack/ &&
+echo test26 >&2 && test -r "$HOME"/.local/share/gitpack/gitpack.log &&
+echo test27 >&2 && test -w "$HOME"/.local/share/gitpack/gitpack.log &&
+echo test28 >&2 && test -r "$HOME"/.local/share/gitpack/status &&
+echo test29 >&2 && test -w "$HOME"/.local/share/gitpack/status &&
+echo test30 >&2 && test -r "$HOME"/.local/share/gitpack/status.bak &&
+echo test31 >&2 && test -w "$HOME"/.local/share/gitpack/status.bak &&
+echo test32 >&2 && test -d "$HOME"/.cache/gitpack/ &&
+echo test33 >&2 && test -d "$HOME"/.cache/gitpack/repo/ &&
+echo test34 >&2 && test -d "$HOME"/.cache/gitpack/repo/github.com/dominiksalvet/ &&
+echo test35 >&2 && test -d "$HOME"/.cache/gitpack/repo/github.com/dominiksalvet/vhdldep/ &&
+
+# downgrade back to 2.1.0
+echo test36 >&2 && gitpack status github.com/dominiksalvet/vhdldep=2.1.0 &&
+gitpack install github.com/dominiksalvet/vhdldep=2.1.0 &&
+gitpack install github.com/dominiksalvet/vhdldep=2.1.0 &&
+gitpack status github.com/dominiksalvet/vhdldep=2.1.0 &&
+gitpack list &&
+# check files
+echo test37 >&2 && test -d "$HOME"/.local/share/gitpack/ &&
+echo test38 >&2 && test -r "$HOME"/.local/share/gitpack/gitpack.log &&
+echo test39 >&2 && test -w "$HOME"/.local/share/gitpack/gitpack.log &&
+echo test40 >&2 && test -r "$HOME"/.local/share/gitpack/status &&
+echo test41 >&2 && test -w "$HOME"/.local/share/gitpack/status &&
+echo test42 >&2 && test -r "$HOME"/.local/share/gitpack/status.bak &&
+echo test43 >&2 && test -w "$HOME"/.local/share/gitpack/status.bak &&
+echo test44 >&2 && test -d "$HOME"/.cache/gitpack/ &&
+echo test45 >&2 && test -d "$HOME"/.cache/gitpack/repo/ &&
+echo test46 >&2 && test -d "$HOME"/.cache/gitpack/repo/github.com/dominiksalvet/ &&
+echo test47 >&2 && test -d "$HOME"/.cache/gitpack/repo/github.com/dominiksalvet/vhdldep/ &&
+
+# uninstall vhdldep
+echo test48 >&2 && gitpack uninstall github.com/dominiksalvet/vhdldep &&
+gitpack uninstall github.com/dominiksalvet/vhdldep &&
+gitpack status github.com/dominiksalvet/vhdldep &&
+# check files
+echo test49 >&2 && test -d "$HOME"/.local/share/gitpack/ &&
+echo test50 >&2 && test -r "$HOME"/.local/share/gitpack/gitpack.log &&
+echo test51 >&2 && test -w "$HOME"/.local/share/gitpack/gitpack.log &&
+echo test52 >&2 && test -r "$HOME"/.local/share/gitpack/status &&
+echo test53 >&2 && test -w "$HOME"/.local/share/gitpack/status &&
+echo test54 >&2 && test -r "$HOME"/.local/share/gitpack/status.bak &&
+echo test55 >&2 && test -w "$HOME"/.local/share/gitpack/status.bak &&
+echo test56 >&2 && test -d "$HOME"/.cache/gitpack/ &&
+echo test57 >&2 && test -d "$HOME"/.cache/gitpack/repo/ &&
+echo test58 >&2 && test -d "$HOME"/.cache/gitpack/repo/github.com/dominiksalvet/ &&
+echo test59 >&2 && test -d "$HOME"/.cache/gitpack/repo/github.com/dominiksalvet/vhdldep/ &&
+
+# intentionally bad URL
+echo test60 >&2 && ! gitpack status github.com/a/b/c 2>&1 &&
+# check files
+echo test61 >&2 && test -d "$HOME"/.local/share/gitpack/ &&
+echo test62 >&2 && test -r "$HOME"/.local/share/gitpack/gitpack.log &&
+echo test63 >&2 && test -w "$HOME"/.local/share/gitpack/gitpack.log &&
+echo test64 >&2 && test -r "$HOME"/.local/share/gitpack/status &&
+echo test65 >&2 && test -w "$HOME"/.local/share/gitpack/status &&
+echo test66 >&2 && test -r "$HOME"/.local/share/gitpack/status.bak &&
+echo test67 >&2 && test -w "$HOME"/.local/share/gitpack/status.bak &&
+echo test68 >&2 && test -d "$HOME"/.cache/gitpack/ &&
+echo test69 >&2 && test -d "$HOME"/.cache/gitpack/repo/ &&
+echo test70 >&2 && test -d "$HOME"/.cache/gitpack/repo/github.com/dominiksalvet/ &&
+echo test71 >&2 && test -d "$HOME"/.cache/gitpack/repo/github.com/dominiksalvet/vhdldep/ &&
+echo test72 >&2 && test ! -d "$HOME"/.cache/gitpack/repo/a/ &&
+
+# clean cache again
+echo test73 >&2 && rm "$HOME"/.local/share/gitpack/status && # should be recovered from a backup
+echo test74 >&2 && gitpack clean &&
+# check files
+echo test75 >&2 && test -d "$HOME"/.local/share/gitpack/ &&
+echo test76 >&2 && test -r "$HOME"/.local/share/gitpack/gitpack.log &&
+echo test77 >&2 && test -w "$HOME"/.local/share/gitpack/gitpack.log &&
+echo test78 >&2 && test -r "$HOME"/.local/share/gitpack/status &&
+echo test79 >&2 && test -w "$HOME"/.local/share/gitpack/status &&
+echo test80 >&2 && test -r "$HOME"/.local/share/gitpack/status.bak &&
+echo test81 >&2 && test -w "$HOME"/.local/share/gitpack/status.bak &&
+echo test82 >&2 && test -d "$HOME"/.cache/gitpack/ &&
+echo test83 >&2 && test ! -d "$HOME"/.cache/gitpack/repo/
--- .ci/tests-usage.sh
+++ .ci/tests-usage.sh
@@ -16,56 +16,56 @@ echo test3 >&2 && gitpack clean && # clean cache
 
 # install an example project, vhdldep, in version 2.1.0 (not the latest release)
 echo test4 >&2 && gitpack_out="$(gitpack status github.com/dominiksalvet/vhdldep=2.1.0)" &&
-echo "$gitpack_out" | grep '^\[nothing\]' &&
-echo test5 >&2 && gitpack_out="$(gitpack install github.com/dominiksalvet/vhdldep=2.1.0)" &&
-echo "$gitpack_out" | grep '^\[install\]' &&
+echo test5 >&2 && echo "$gitpack_out" | grep '^\[nothing\]' &&
 echo test6 >&2 && gitpack_out="$(gitpack install github.com/dominiksalvet/vhdldep=2.1.0)" &&
-echo "$gitpack_out" | grep '^\[installed\]' &&
-echo test7 >&2 && gitpack_out="$(gitpack status github.com/dominiksalvet/vhdldep=2.1.0)" &&
-echo "$gitpack_out" | grep '^\[ok\]' &&
-echo test8 >&2 && gitpack_out="$(gitpack list)" &&
-echo "$gitpack_out" | grep -F 'github.com/dominiksalvet/vhdldep' &&
+echo test7 >&2 && echo "$gitpack_out" | grep '^\[install\]' &&
+echo test8 >&2 && gitpack_out="$(gitpack install github.com/dominiksalvet/vhdldep=2.1.0)" &&
+echo test9 >&2 && echo "$gitpack_out" | grep '^\[installed\]' &&
+echo test10 >&2 && gitpack_out="$(gitpack status github.com/dominiksalvet/vhdldep=2.1.0)" &&
+echo test11 >&2 && echo "$gitpack_out" | grep '^\[ok\]' &&
+echo test12 >&2 && gitpack_out="$(gitpack list)" &&
+echo test13 >&2 && echo "$gitpack_out" | grep -F 'github.com/dominiksalvet/vhdldep' &&
 
 # update to the latest version
-echo test9 >&2 && gitpack_out="$(gitpack status github.com/dominiksalvet/vhdldep)" &&
-echo "$gitpack_out" | grep '^\[older\]' &&
-echo test10 >&2 && rm "$HOME"/.local/share/gitpack/status && # should be recovered from a backup
-echo test11 >&2 && gitpack_out="$(gitpack install github.com/dominiksalvet/vhdldep)" &&
-echo "$gitpack_out" | grep '^\[update\]' &&
-echo test12 >&2 && rm "$HOME"/.local/share/gitpack/status &&
-echo test13 >&2 && gitpack_out="$(gitpack install github.com/dominiksalvet/vhdldep)" &&
-echo "$gitpack_out" | grep '^\[installed\]' &&
-echo test14 >&2 && rm "$HOME"/.local/share/gitpack/status &&
-echo test15 >&2 && gitpack_out="$(gitpack status github.com/dominiksalvet/vhdldep)" &&
-echo "$gitpack_out" | grep '^\[ok\]' &&
-echo test16 >&2 && rm "$HOME"/.local/share/gitpack/status &&
-echo test17 >&2 && gitpack_out="$(gitpack list)" &&
-echo "$gitpack_out" | grep -F 'github.com/dominiksalvet/vhdldep' &&
-echo test18 >&2 && rm "$HOME"/.local/share/gitpack/status &&
+echo test14 >&2 && gitpack_out="$(gitpack status github.com/dominiksalvet/vhdldep)" &&
+echo test15 >&2 && echo "$gitpack_out" | grep '^\[older\]' &&
+echo test16 >&2 && rm "$HOME"/.local/share/gitpack/status && # should be recovered from a backup
+echo test17 >&2 && gitpack_out="$(gitpack install github.com/dominiksalvet/vhdldep)" &&
+echo test18 >&2 && echo "$gitpack_out" | grep '^\[update\]' &&
+echo test19 >&2 && rm "$HOME"/.local/share/gitpack/status &&
+echo test20 >&2 && gitpack_out="$(gitpack install github.com/dominiksalvet/vhdldep)" &&
+echo test21 >&2 && echo "$gitpack_out" | grep '^\[installed\]' &&
+echo test22 >&2 && rm "$HOME"/.local/share/gitpack/status &&
+echo test23 >&2 && gitpack_out="$(gitpack status github.com/dominiksalvet/vhdldep)" &&
+echo test24 >&2 && echo "$gitpack_out" | grep '^\[ok\]' &&
+echo test25 >&2 && rm "$HOME"/.local/share/gitpack/status &&
+echo test26 >&2 && gitpack_out="$(gitpack list)" &&
+echo test27 >&2 && echo "$gitpack_out" | grep -F 'github.com/dominiksalvet/vhdldep' &&
+echo test28 >&2 && rm "$HOME"/.local/share/gitpack/status &&
 
 # downgrade back to 2.1.0
-echo test19 >&2 && gitpack_out="$(gitpack status github.com/dominiksalvet/vhdldep=2.1.0)" &&
-echo "$gitpack_out" | grep '^\[newer\]' &&
-echo test20 >&2 && gitpack_out="$(gitpack install github.com/dominiksalvet/vhdldep=2.1.0)" &&
-echo "$gitpack_out" | grep '^\[downgrade\]' &&
-echo test21 >&2 && gitpack_out="$(gitpack install github.com/dominiksalvet/vhdldep=2.1.0)" &&
-echo "$gitpack_out" | grep '^\[installed\]' &&
-echo test22 >&2 && gitpack_out="$(gitpack status github.com/dominiksalvet/vhdldep=2.1.0)" &&
-echo "$gitpack_out" | grep '^\[ok\]' &&
-echo test23 >&2 && gitpack_out="$(gitpack list)" &&
-echo "$gitpack_out" | grep -F 'github.com/dominiksalvet/vhdldep' &&
+echo test29 >&2 && gitpack_out="$(gitpack status github.com/dominiksalvet/vhdldep=2.1.0)" &&
+echo test30 >&2 && echo "$gitpack_out" | grep '^\[newer\]' &&
+echo test31 >&2 && gitpack_out="$(gitpack install github.com/dominiksalvet/vhdldep=2.1.0)" &&
+echo test32 >&2 && echo "$gitpack_out" | grep '^\[downgrade\]' &&
+echo test33 >&2 && gitpack_out="$(gitpack install github.com/dominiksalvet/vhdldep=2.1.0)" &&
+echo test34 >&2 && echo "$gitpack_out" | grep '^\[installed\]' &&
+echo test35 >&2 && gitpack_out="$(gitpack status github.com/dominiksalvet/vhdldep=2.1.0)" &&
+echo test36 >&2 && echo "$gitpack_out" | grep '^\[ok\]' &&
+echo test37 >&2 && gitpack_out="$(gitpack list)" &&
+echo test38 >&2 && echo "$gitpack_out" | grep -F 'github.com/dominiksalvet/vhdldep' &&
 
 # uninstall vhdldep
-echo test24 >&2 && gitpack_out="$(gitpack uninstall github.com/dominiksalvet/vhdldep)" &&
-echo "$gitpack_out" | grep '^\[uninstall\]' &&
-echo test25 >&2 && gitpack_out="$(gitpack uninstall github.com/dominiksalvet/vhdldep)" &&
-echo "$gitpack_out" | grep '^\[uninstalled\]' &&
-echo test26 >&2 && gitpack_out="$(gitpack status github.com/dominiksalvet/vhdldep)" &&
-echo "$gitpack_out" | grep '^\[nothing\]' &&
+echo test39 >&2 && gitpack_out="$(gitpack uninstall github.com/dominiksalvet/vhdldep)" &&
+echo test40 >&2 && echo "$gitpack_out" | grep '^\[uninstall\]' &&
+echo test41 >&2 && gitpack_out="$(gitpack uninstall github.com/dominiksalvet/vhdldep)" &&
+echo test42 >&2 && echo "$gitpack_out" | grep '^\[uninstalled\]' &&
+echo test43 >&2 && gitpack_out="$(gitpack status github.com/dominiksalvet/vhdldep)" &&
+echo test44 >&2 && echo "$gitpack_out" | grep '^\[nothing\]' &&
 
 # intentionally bad URL
-echo test27 >&2 && gitpack_out="$(! gitpack status github.com/a/b/c 2>&1)" &&
-echo "$gitpack_out" | grep '^<ERROR>' &&
+echo test45 >&2 && gitpack_out="$(! gitpack status github.com/a/b/c 2>&1)" &&
+echo test46 >&2 && echo "$gitpack_out" | grep '^<ERROR>' &&
 
 # clean cache again
-echo test28 >&2 && gitpack clean
+echo test47 >&2 && gitpack clean
