2ff7991e7914375de721caab3b8e1e80cb77b98b
Dominik Salvet <dominik.salvet@gmail.com>
2019-09-18 15:29:57 +0200

    Rename clone_repo() to refresh_repo()

--- .ci/tests-logging.sh
+++ .ci/tests-logging.sh
@@ -32,7 +32,7 @@ echo test10 >&2 && test "$(grep -c '\[backup\]' "$HOME"/.local/share/gitpack/git
 echo test11 >&2 && test "$(grep -c '\[exit\]' "$HOME"/.local/share/gitpack/gitpack.log)" -eq 6 &&
 echo test12 >&2 && test "$(grep -c '\[action\]' "$HOME"/.local/share/gitpack/gitpack.log)" -eq 4 &&
 echo test13 >&2 && test "$(grep -c '\[url\]' "$HOME"/.local/share/gitpack/gitpack.log)" -eq 4 &&
-echo test14 >&2 && test "$(grep -c '\[clone\]' "$HOME"/.local/share/gitpack/gitpack.log)" -eq 4 &&
+echo test14 >&2 && test "$(grep -c '\[refresh\]' "$HOME"/.local/share/gitpack/gitpack.log)" -eq 4 &&
 echo test15 >&2 && test "$(grep -c '\[execute\]' "$HOME"/.local/share/gitpack/gitpack.log)" -eq 4 &&
 echo test16 >&2 && test "$(grep -c '\[get\]' "$HOME"/.local/share/gitpack/gitpack.log)" -eq 4 &&
 echo test17 >&2 && test "$(grep -c '\[cp\]' "$HOME"/.local/share/gitpack/gitpack.log)" -eq 1 &&
@@ -53,7 +53,7 @@ echo test24 >&2 && test "$(grep -c '\[backup\]' "$HOME"/.local/share/gitpack/git
 echo test25 >&2 && test "$(grep -c '\[exit\]' "$HOME"/.local/share/gitpack/gitpack.log)" -eq 11 &&
 echo test26 >&2 && test "$(grep -c '\[action\]' "$HOME"/.local/share/gitpack/gitpack.log)" -eq 8 &&
 echo test27 >&2 && test "$(grep -c '\[url\]' "$HOME"/.local/share/gitpack/gitpack.log)" -eq 8 &&
-echo test28 >&2 && test "$(grep -c '\[clone\]' "$HOME"/.local/share/gitpack/gitpack.log)" -eq 8 &&
+echo test28 >&2 && test "$(grep -c '\[refresh\]' "$HOME"/.local/share/gitpack/gitpack.log)" -eq 8 &&
 echo test29 >&2 && test "$(grep -c '\[execute\]' "$HOME"/.local/share/gitpack/gitpack.log)" -eq 8 &&
 echo test30 >&2 && test "$(grep -c '\[get\]' "$HOME"/.local/share/gitpack/gitpack.log)" -eq 8 &&
 echo test31 >&2 && test "$(grep -c '\[cp\]' "$HOME"/.local/share/gitpack/gitpack.log)" -eq 2 &&
@@ -76,7 +76,7 @@ echo test40 >&2 && test "$(grep -c '\[backup\]' "$HOME"/.local/share/gitpack/git
 echo test41 >&2 && test "$(grep -c '\[exit\]' "$HOME"/.local/share/gitpack/gitpack.log)" -eq 16 &&
 echo test42 >&2 && test "$(grep -c '\[action\]' "$HOME"/.local/share/gitpack/gitpack.log)" -eq 12 &&
 echo test43 >&2 && test "$(grep -c '\[url\]' "$HOME"/.local/share/gitpack/gitpack.log)" -eq 12 &&
-echo test44 >&2 && test "$(grep -c '\[clone\]' "$HOME"/.local/share/gitpack/gitpack.log)" -eq 12 &&
+echo test44 >&2 && test "$(grep -c '\[refresh\]' "$HOME"/.local/share/gitpack/gitpack.log)" -eq 12 &&
 echo test45 >&2 && test "$(grep -c '\[execute\]' "$HOME"/.local/share/gitpack/gitpack.log)" -eq 12 &&
 echo test46 >&2 && test "$(grep -c '\[get\]' "$HOME"/.local/share/gitpack/gitpack.log)" -eq 12 &&
 echo test47 >&2 && test "$(grep -c '\[cp\]' "$HOME"/.local/share/gitpack/gitpack.log)" -eq 3 &&
@@ -97,7 +97,7 @@ echo test56 >&2 && test "$(grep -c '\[backup\]' "$HOME"/.local/share/gitpack/git
 echo test57 >&2 && test "$(grep -c '\[exit\]' "$HOME"/.local/share/gitpack/gitpack.log)" -eq 19 &&
 echo test58 >&2 && test "$(grep -c '\[action\]' "$HOME"/.local/share/gitpack/gitpack.log)" -eq 15 &&
 echo test59 >&2 && test "$(grep -c '\[url\]' "$HOME"/.local/share/gitpack/gitpack.log)" -eq 15 &&
-echo test60 >&2 && test "$(grep -c '\[clone\]' "$HOME"/.local/share/gitpack/gitpack.log)" -eq 15 &&
+echo test60 >&2 && test "$(grep -c '\[refresh\]' "$HOME"/.local/share/gitpack/gitpack.log)" -eq 15 &&
 echo test61 >&2 && test "$(grep -c '\[execute\]' "$HOME"/.local/share/gitpack/gitpack.log)" -eq 15 &&
 echo test62 >&2 && test "$(grep -c '\[get\]' "$HOME"/.local/share/gitpack/gitpack.log)" -eq 15 &&
 echo test63 >&2 && test "$(grep -c '\[cp\]' "$HOME"/.local/share/gitpack/gitpack.log)" -eq 3 &&
@@ -116,7 +116,7 @@ echo test72 >&2 && test "$(grep -c '\[backup\]' "$HOME"/.local/share/gitpack/git
 echo test73 >&2 && test "$(grep -c '\[exit\]' "$HOME"/.local/share/gitpack/gitpack.log)" -eq 20 &&
 echo test74 >&2 && test "$(grep -c '\[action\]' "$HOME"/.local/share/gitpack/gitpack.log)" -eq 16 &&
 echo test75 >&2 && test "$(grep -c '\[url\]' "$HOME"/.local/share/gitpack/gitpack.log)" -eq 16 &&
-echo test76 >&2 && test "$(grep -c '\[clone\]' "$HOME"/.local/share/gitpack/gitpack.log)" -eq 15 &&
+echo test76 >&2 && test "$(grep -c '\[refresh\]' "$HOME"/.local/share/gitpack/gitpack.log)" -eq 15 &&
 echo test77 >&2 && test "$(grep -c '\[execute\]' "$HOME"/.local/share/gitpack/gitpack.log)" -eq 15 &&
 echo test78 >&2 && test "$(grep -c '\[get\]' "$HOME"/.local/share/gitpack/gitpack.log)" -eq 15 &&
 echo test79 >&2 && test "$(grep -c '\[cp\]' "$HOME"/.local/share/gitpack/gitpack.log)" -eq 3 &&
@@ -136,7 +136,7 @@ echo test89 >&2 && test "$(grep -c '\[backup\]' "$HOME"/.local/share/gitpack/git
 echo test90 >&2 && test "$(grep -c '\[exit\]' "$HOME"/.local/share/gitpack/gitpack.log)" -eq 21 &&
 echo test91 >&2 && test "$(grep -c '\[action\]' "$HOME"/.local/share/gitpack/gitpack.log)" -eq 16 &&
 echo test92 >&2 && test "$(grep -c '\[url\]' "$HOME"/.local/share/gitpack/gitpack.log)" -eq 16 &&
-echo test93 >&2 && test "$(grep -c '\[clone\]' "$HOME"/.local/share/gitpack/gitpack.log)" -eq 15 &&
+echo test93 >&2 && test "$(grep -c '\[refresh\]' "$HOME"/.local/share/gitpack/gitpack.log)" -eq 15 &&
 echo test94 >&2 && test "$(grep -c '\[execute\]' "$HOME"/.local/share/gitpack/gitpack.log)" -eq 15 &&
 echo test95 >&2 && test "$(grep -c '\[get\]' "$HOME"/.local/share/gitpack/gitpack.log)" -eq 15 &&
 echo test96 >&2 && test "$(grep -c '\[cp\]' "$HOME"/.local/share/gitpack/gitpack.log)" -eq 3 &&
