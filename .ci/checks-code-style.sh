b7f0f952bc4161d5da7e5882456e3c10d117c21d
Dominik Salvet <dominik.salvet@gmail.com>
2019-09-18 23:45:00 +0200

    Include gitpack-completion.bash to CI completely

--- .ci/checks-code-style.sh
+++ .ci/checks-code-style.sh
@@ -5,39 +5,20 @@
 # github.com/dominiksalvet/gitpack
 #-------------------------------------------------------------------------------
 # DESCRIPTION:
-#   This test checks whether the main GitPack script meets a basic code style.
+#   This test checks whether the GitPack source code meets a defined code style.
 #-------------------------------------------------------------------------------
 
 #-------------------------------------------------------------------------------
-# CONFIGURATION
+# DEFINITIONS
 #-------------------------------------------------------------------------------
 
 MAX_LINE_LENGTH=80 # maximum number of characters per each line
 MAX_FUNCTION_LENGTH=20 # maximum number of lines per each function
 IGNORED_FUNCTIONS='init_constants init_strings' # function length exceptions
 
-#-------------------------------------------------------------------------------
-# LINE LENGTH
-#-------------------------------------------------------------------------------
-
-echo test1 >&2
-lineno=0 # initialize line counter
-while IFS= read -r line; do
-    lineno=$((lineno + 1)) # increment line counter
-    if [ "${#line}" -gt "$MAX_LINE_LENGTH" ]; then # check line length
-        echo "src/gitpack:$lineno line has more than $MAX_LINE_LENGTH characters" >&2
-        return 1
-    fi
-done < src/gitpack
-
-#-------------------------------------------------------------------------------
-# FUNCTION LENGTH
-#-------------------------------------------------------------------------------
-
-echo test2 >&2
-# transforms a given shell script into '<function> <lines>' per line output
+# AWK prints '<lineno> <function> <lines>' per line for shell script file inputs
 # shellcheck disable=SC2016
-AWK_PROGRAM='{ if (/\(\)/) {
+FUNC_SUMMARY='{ if (/\(\)/) {
     printf NR" "$1" "
     lines=1
 } else if (/^(\) )?\}$/) {
@@ -46,22 +27,61 @@ AWK_PROGRAM='{ if (/\(\)/) {
     lines++
 } }'
 
-# use '(' as a field separator to make creating the final output easier
-awk_out="$(awk -F '(' "$AWK_PROGRAM" src/gitpack)" &&
-echo "$awk_out" |
-while read -r line; do
-    lineno="$(echo "$line" | cut -f 1 -d ' ')" && # extract line number
-    function="$(echo "$line" | cut -f 2 -d ' ')" && # extract function name
-    lines="$(echo "$line" | cut -f 3 -d ' ')" || return # extract function length in lines
+#-------------------------------------------------------------------------------
+# LINE LENGTH
+#-------------------------------------------------------------------------------
 
-    for ignored_function in $IGNORED_FUNCTIONS; do # exclude ignored functions
-        if [ "$function" = "$ignored_function" ]; then
-            lines="$MAX_FUNCTION_LENGTH"
+# DESCRIPTION:
+#   Tests whether each line of a given file has at most a defined number of
+#   characters.
+# PARAMETERS:
+#   $1 - file path
+test_line_lenght() {
+    lineno=0 # initialize line counter
+    while IFS= read -r line; do
+        lineno=$((lineno + 1)) # increment line counter
+        if [ "${#line}" -gt "$MAX_LINE_LENGTH" ]; then # check line length
+            echo "$1:$lineno line has more than $MAX_LINE_LENGTH characters" >&2
+            return 1
+        fi
+    done < "$1"
+}
+
+#-------------------------------------------------------------------------------
+# FUNCTION LENGTH
+#-------------------------------------------------------------------------------
+
+# DESCRIPTION:
+#   Tests whether each function of a given script file has at most a defined
+#   number of lines.
+# PARAMETERS:
+#   $1 - script path
+test_func_lenght() {
+    # use '(' as a field separator to make creating the final output easier
+    awk_out="$(awk -F '(' "$FUNC_SUMMARY" "$1")" &&
+    echo "$awk_out" | while read -r line; do # for each function summary
+        lineno="$(echo "$line" | cut -f 1 -d ' ')" && # extract line number
+        function="$(echo "$line" | cut -f 2 -d ' ')" && # extract function name
+        lines="$(echo "$line" | cut -f 3 -d ' ')" || return # extract function length in lines
+
+        for ignored_function in $IGNORED_FUNCTIONS; do # exclude ignored functions
+            if [ "$function" = "$ignored_function" ]; then
+                lines="$MAX_FUNCTION_LENGTH"
+            fi
+        done
+
+        if [ "$lines" -gt "$MAX_FUNCTION_LENGTH" ]; then # check function length
+            echo "$1:$lineno $function() has more than $MAX_FUNCTION_LENGTH lines" >&2
+            return 1
         fi
     done
+}
 
-    if [ "$lines" -gt "$MAX_FUNCTION_LENGTH" ]; then # check function length
-        echo "src/gitpack:$lineno $function() has more than $MAX_FUNCTION_LENGTH lines" >&2
-        return 1
-    fi
-done
+#-------------------------------------------------------------------------------
+# TESTING
+#-------------------------------------------------------------------------------
+
+echo test1 >&2 && test_line_lenght src/gitpack &&
+echo test2 >&2 && test_func_lenght src/gitpack &&
+echo test3 >&2 && test_line_lenght src/gitpack-completion.bash &&
+echo test4 >&2 && test_func_lenght src/gitpack-completion.bash
