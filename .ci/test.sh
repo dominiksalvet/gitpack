95f8ed41169ba428079cd9d131b6b617bb5e352a
Dominik Salvet <dominik.salvet@gmail.com>
2019-10-05 01:18:34 +0200

    Print another variables in CI...

--- .ci/test.sh
+++ .ci/test.sh
@@ -15,6 +15,12 @@
 # Checks whether the current commit of GitPack can install itself globally.
 #-------------------------------------------------------------------------------
 
+# print variables
+echo "$TRAVIS_COMMIT"
+git rev-parse FETCH_HEAD
+git rev-parse master
+git rev-parse origin/offline-mode
+
 # copy current repository to global GitPack cache
 echo global1 >&2 && sudo mkdir -p /var/cache/gitpack/repo/github.com/dominiksalvet/gitpack/ &&
 echo global2 >&2 && sudo cp -R ./ /var/cache/gitpack/repo/github.com/dominiksalvet/gitpack/ &&
