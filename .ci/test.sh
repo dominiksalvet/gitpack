79aadd124e9fbb65df993c370e51a6810201feb7
Dominik Salvet <dominik.salvet@gmail.com>
2019-10-09 22:47:44 +0200

    Make the error detectection in CI silent

--- .ci/test.sh
+++ .ci/test.sh
@@ -20,7 +20,7 @@ if [ "$tester_status" -eq 0 ]; then # if tests were successful
     # check if there is an error message (ignoring a diagnostic message format)
     while read -r line; do
         # if it does not match the diagnostic format, it is an error message
-        if ! echo "$line" | grep '^[[:alpha:]]\+[[:digit:]]\+$'; then
+        if ! echo "$line" | grep -q '^[[:alpha:]]\+[[:digit:]]\+$'; then
             echo "error detected: $line" >&2; exit 1
         fi
     done < ~/gitpack-tester-output &&
