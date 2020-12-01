19d1811b81914ee3b40637fdbd13c278d0860c35
Dominik Salvet <dominik.salvet@gmail.com>
2020-12-01 22:12:55 +0100

    fix git describe in test

--- test/run.sh
+++ test/run.sh
@@ -37,8 +37,7 @@ export SHORT_HASH &&
 
 export GITPACK='sh -x src/gitpack' && # GitPack is run with tracing enabled
 export URL=github.com/dominiksalvet/gitpack &&
-VERSION="$(git describe --tags --abbrev=0)" && # latest GitPack version
-export VERSION &&
+export VERSION=0.7.0 && # latest GitPack version
 export VERSION_HASH=d5a9f75a89eefbd5316b5abe956d28d3a5358327 &&
 export VERSION_SHORT_HASH=d5a9f75 &&
 export OLD_VERSION=0.1.0 &&
