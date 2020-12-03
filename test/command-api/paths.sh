1632be43a9492f211df9741c8129eee0c47f90bd
Dominik Salvet <dominik.salvet@gmail.com>
2020-12-03 22:01:14 +0100

    fix paths test

--- test/command-api/paths.sh
+++ test/command-api/paths.sh
@@ -10,7 +10,7 @@ user_id="$(id -u)" &&
 out="$($GITPACK paths)" &&
 test "$out" = "state-dir $HOME/.local/share/gitpack
 cache-dir $HOME/.cache/gitpack
-lock-path /var/lock/gitpack-$user_id"
+lock-path /var/lock/gitpack-$user_id" &&
 
 # global paths
 root_id="$(sudo id -u)" &&
