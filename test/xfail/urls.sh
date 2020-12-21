4cf56b2dc2ef9d3928ea50775d10183d64f5d39f
Dominik Salvet <dominik.salvet@gmail.com>
2020-12-21 18:49:09 +0100

    update xfail URLs test

--- test/xfail/urls.sh
+++ test/xfail/urls.sh
@@ -8,23 +8,20 @@
 # shellcheck disable=SC2034
 
 out1="$(! $GITPACK status '')"
-out2="$(! $GITPACK status 'bad url')"
-out3="$(! $GITPACK status 'bad
+out2="$(! $GITPACK status ' ')"
+out3="$(! $GITPACK status 'bad url')"
+out4="$(! $GITPACK status 'bad
 url')"
-out4="$(! $GITPACK status .)"
-out5="$(! $GITPACK status ..)"
-out6="$(! $GITPACK status "$URL"/.)"
-out7="$(! $GITPACK status "$URL"/..)"
-out8="$(! $GITPACK status ./"$URL")"
-out9="$(! $GITPACK status ../"$URL")"
-out10="$(! $GITPACK status badurl/./badurl)"
-out11="$(! $GITPACK status badurl/../badurl)"
 
-out12="$(! $GITPACK status 'github.com/dominik
+out5="$(! $GITPACK status .)"
+out6="$(! $GITPACK status ..)"
+out7="$(! $GITPACK status "$URL"/.)"
+out8="$(! $GITPACK status "$URL"/..)"
+out9="$(! $GITPACK status ./"$URL")"
+out10="$(! $GITPACK status ../"$URL")"
+out11="$(! $GITPACK status bad/./url)"
+out12="$(! $GITPACK status bad/../url)"
+
+out13="$(! $GITPACK status 'github.com/dominik
 salvet/gitpack')"
-out13="$(! $GITPACK status "$URL=
-HEAD")"
-out14="$(! $GITPACK status "$URL=HE
-AD")"
-out15="$(! $GITPACK status "$URL=HEAD
-")"
+out14="$(! $GITPACK status "$URL=nonexistent")"
