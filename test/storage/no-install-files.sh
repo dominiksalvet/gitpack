b1667f5b1b631fcd8bb18a2c86a11c802b3ec58e
Dominik Salvet <dominik.salvet@gmail.com>
2020-12-28 23:27:00 +0100

    update no-install-files.sh

--- test/storage/no-install-files.sh
+++ test/storage/no-install-files.sh
@@ -8,5 +8,7 @@
 test ! -e "$HOME/.local/bin/gitpack"
 test ! -e "$HOME/.bash_completion.d/gitpack-completion"
 
-test ! -e /usr/local/bin/gitpack
-test ! -e /etc/bash_completion.d/gitpack-completion
+if [ "$USE_SUDO" = true ]; then
+    sudo test ! -e /usr/local/bin/gitpack
+    sudo test ! -e /etc/bash_completion.d/gitpack-completion
+fi
