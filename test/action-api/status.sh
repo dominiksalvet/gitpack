4ca05c10a9903251826a8d0addc25daf6808fffa
Dominik Salvet <dominik.salvet@gmail.com>
2021-01-08 19:51:09 +0100

    add -n option to tests

--- test/action-api/status.sh
+++ test/action-api/status.sh
@@ -26,23 +26,31 @@ out5="$($GITPACK -f status "$URL")"
 test "$out5" = ">>> running status for $URL
 <<< not installed; candidate is $VERSION"
 
-out6="$($GITPACK -h status "$URL")"
+out6="$($GITPACK -n status "$URL")"
 test "$out6" = ">>> running status for $URL
+<<< not installed; candidate is $VERSION"
+
+out7="$($GITPACK -h status "$URL")"
+test "$out7" = ">>> running status for $URL
 <<< not installed; candidate is $VERSION_SHORT_HASH"
 
-out7="$($GITPACK -H status "$URL")"
-test "$out7" = ">>> running status for $URL
+out8="$($GITPACK -H status "$URL")"
+test "$out8" = ">>> running status for $URL
 <<< not installed; candidate is $VERSION_HASH"
 
-out8="$($GITPACK -r status "https://$URL.git")" # raw URL mode
-test "$out8" = ">>> running status for https://$URL.git
+out9="$($GITPACK -r status "https://$URL.git")" # raw URL mode
+test "$out9" = ">>> running status for https://$URL.git
 <<< not installed; candidate is $VERSION"
 
 # multiple options
-out9="$($GITPACK -fh status "$URL")"
-test "$out9" = ">>> running status for $URL
+out10="$($GITPACK -fh status "$URL")"
+test "$out10" = ">>> running status for $URL
 <<< not installed; candidate is $VERSION_SHORT_HASH"
 
-out10="$($GITPACK -Hr status "https://$URL.git")"
-test "$out10" = ">>> running status for https://$URL.git
+out11="$($GITPACK -Hr status "https://$URL.git")"
+test "$out11" = ">>> running status for https://$URL.git
 <<< not installed; candidate is $VERSION_HASH"
+
+out12="$($GITPACK -hnr status "https://$URL.git")"
+test "$out12" = ">>> running status for https://$URL.git
+<<< not installed; candidate is $VERSION_SHORT_HASH"
