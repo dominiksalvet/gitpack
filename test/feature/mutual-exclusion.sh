87f1b5b76340c431f8763bacd3759fdd9b9caa47
Dominik Salvet <dominik.salvet@gmail.com>
2021-01-29 16:22:57 +0100

    fix typo in mutual exclusion test

--- test/feature/mutual-exclusion.sh
+++ test/feature/mutual-exclusion.sh
@@ -22,10 +22,10 @@ wait # wait for both processes to finish
 status_1="$(cat /tmp/gitpack-status-1)"
 out3="$($GITPACK status "$URL=$VERSION")"
 if [ "$status_1" -eq 0 ]; then
-    test "$out3" = ">>> running status for $URL
+    test "$out3" = ">>> running status for $URL=$VERSION
 <<< candidate $VERSION already installed"
 elif [ "$status_1" -eq 64 ]; then
-    test "$out3" = ">>> running status for $URL
+    test "$out3" = ">>> running status for $URL=$VERSION
 <<< not installed; candidate is $VERSION"
 else
     false # no other exit statuses accepted
@@ -34,10 +34,10 @@ fi
 status_2="$(cat /tmp/gitpack-status-2)"
 out4="$($GITPACK status "$EXTRA_URL=$EXTRA_VERSION")"
 if [ "$status_2" -eq 0 ]; then
-    test "$out4" = ">>> running status for $EXTRA_URL
+    test "$out4" = ">>> running status for $EXTRA_URL=$EXTRA_VERSION
 <<< candidate $EXTRA_VERSION already installed"
 elif [ "$status_2" -eq 64 ]; then
-    test "$out4" = ">>> running status for $EXTRA_URL
+    test "$out4" = ">>> running status for $EXTRA_URL=$EXTRA_VERSION
 <<< not installed; candidate is $EXTRA_VERSION"
     test [ "$status_1" -ne 64 ] # at least one must acquire lock
 else
