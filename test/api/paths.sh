a7e8580162128128760352361a621b43b07926cb
Dominik Salvet <dominik.salvet@gmail.com>
2021-03-13 15:36:20 +0100

    lockfiles now have .lock extension

--- test/api/paths.sh
+++ test/api/paths.sh
@@ -10,7 +10,7 @@ user_id="$(id -u)"
 out1="$($GITPACK paths)"
 test "$out1" = "state-dir $LOCAL_STATE_DIR
 cache-dir $LOCAL_CACHE_DIR
-lock-path $LOCK_DIR/gitpack-$user_id"
+lock-path $LOCK_DIR/gitpack-$user_id.lock"
 
 if [ "$USE_SUDO" = true ]; then
     # global paths
@@ -18,5 +18,5 @@ if [ "$USE_SUDO" = true ]; then
     out2="$($SUDO_GITPACK paths)"
     test "$out2" = "state-dir $GLOBAL_STATE_DIR
 cache-dir $GLOBAL_CACHE_DIR
-lock-path $LOCK_DIR/gitpack-$root_id"
+lock-path $LOCK_DIR/gitpack-$root_id.lock"
 fi
